8866203aba68e035c96417ac768f8502bdd154e3
aakanksha-123 <94225817+aakanksha-123@users.noreply.github.com>
2022-02-21 22:43:53 +0530

    Multilevel Inheritance

---
+++ 30.Multilevel Inheritance.cpp
@@ -0,0 +1,46 @@
+#include <iostream>  
+using namespace std; 
+
+class A{
+	int a;
+	public:
+		void getA(){
+			cout<<"Enter value of A: ";
+			cin>>a;
+		}
+		void putA(){
+		cout<<"a="<<a;
+		}
+};
+class B:public A{
+  	int b;
+  	public:
+  		void getB(){
+  			cout<<"Enter value of B: ";
+  			cin>>b;
+		  }
+		void putB(){
+			cout<<"\nb="<<b;
+		}
+  };
+  class C:public B{
+  	int c;
+  	public:
+  		void getC(){
+  			cout<<"Enter value of C: ";
+  			cin>>c;
+		  }
+		void putC(){
+			cout<<"\nc="<<c;
+		}
+  };
+  int main(){
+  	C c;
+  	c.getA();
+  	c.getB();
+  	c.getC();
+  	c.putA();
+  	c.putB();
+  	c.putC();
+  }
+  
